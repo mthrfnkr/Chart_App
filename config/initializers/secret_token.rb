e0a2cb798c3ee11003473fdef9814bcc8112bc65
Kornelia <kornelia987@gmail.com>
2013-12-01 21:49:20 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChartApp::Application.config.secret_key_base = '3b49ec599636db5c9d66fd37bd294c1053200544ec337b7f6d958461090748920f28ce8537cc0178d7fd7c9fe5189425ad33caef6f22647916bd1bed541e869e'
